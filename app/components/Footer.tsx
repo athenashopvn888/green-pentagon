0036d4615ffc2270e309de242af8d01b4b12ba2d
athenashopvn888 <athenashopvn888@gmail.com>
2026-08-09 17:59:08 +0700

    feat: add POD3 delivery experience

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -57,7 +57,7 @@ export default function Footer() {
               <Link href="/items/vapes">Vape Pens</Link>
               <Link href="/resources">Resources</Link>
               <Link href="/faq">FAQ</Link>
-              <Link href="/delivery">Delivery (Coming Soon)</Link>
+              <Link href="/delivery">Delivery Menu</Link>
               <Link href="/info/queen-west-weed-dispensary">
                 Queen West Dispensary
               </Link>
